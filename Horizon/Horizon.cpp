748b13123d584f8ef89be8eb668640255b6181dc
pogsworth <pjohnst@msn.com>
2024-08-05 00:02:55 -0600

    More progress on Horizon indicator, still needs work

--- Horizon/Horizon.cpp
+++ Horizon/Horizon.cpp
@@ -15,6 +15,16 @@ class Horizon : public Jogo::JogoApp
 	float originy = 0;
 	s32 scroll = 0;
 	float scale = 1.0;
+	u32 SkyColor = 0x0080A0;
+	u32 GroundColor = 0x806000;
+
+	// parameters to convert to and from Screen and Pitch space
+	const s32 HorizonWidth = 500;
+	const s32 HorizonHeight = 500;
+	const float ScreenToPitchScale = 60.0f / HorizonHeight;
+	const float PitchToScreenScale = HorizonHeight / 60.f;
+	float PitchOriginScreenSpaceX;
+	float PitchOriginScreenSpaceY;
 
 public:
 	Horizon() {}
@@ -24,9 +34,48 @@ public:
 
 	bool Tick(float DT /* do we need anything else passed in here?*/) override
 	{
-		roll += DT;
-
+		if (Jogo::IsKeyPressed(KEY_RIGHT))
+		{
+			roll += 30.0f * DT;
+		}
+		if (Jogo::IsKeyPressed(KEY_LEFT))
+		{
+			roll -= 30.0f * DT;
+		}
+		float localupx = Jogo::sine(roll);
+		float localupy = Jogo::cosine(roll);
+		static float timer = 0;
+		timer += DT;
+		char timerString[32];
+		Jogo::ftoa(timer, timerString, sizeof(timerString));
+		char* p = timerString;
+		for (s32 i = 0; i < 32; i++, p++)
+		{
+			if (!*p)
+			{
+				*p++ = '\n';
+				*p = 0;
+				break;
+			}
+		}
+		Jogo::DebugOut(timerString);
+		if (Jogo::IsKeyPressed(KEY_UP))
+		{
+			pitch += 20.0f * localupy * DT;
+		}
+		if (Jogo::IsKeyPressed(KEY_DOWN))
+		{
+			pitch -= 20.0f * localupy * DT;
+		}
 		return Done;
+	}
+
+	void KeyDown(u32 key)
+	{
+		if (key == KEY_ESC)
+		{
+			Done = true;
+		}
 	}
 
 	void MouseDown(s32 x, s32 y, u32 buttons)
@@ -60,14 +109,114 @@ public:
 		scale *= 1.0f + (float)scroll / 20;
 	}
 
+	template<class T>
+	T Abs(T input)
+	{
+		return input >= 0 ? input : -input;
+	}
+
+	void VectorToPitchOriginScreenSpace(float& x, float& y)
+	{
+		x = x - PitchOriginScreenSpaceX;
+		y = y - PitchOriginScreenSpaceY;
+	}
+
 	void DrawHorizon(Bitmap::Rect& frame, float pitch, float roll)
 	{
-		float x = frame.x + frame.w / 2;
-		float y = frame.y + frame.h / 2;
-		float r = frame.w / 2;
-		float x1 = r * Jogo::cosine(roll);
-		float y1 = r * Jogo::sine(roll);
-		BackBuffer.DrawLine((int)(x + x1), (int)(y - y1), (int)(x - x1), (int)(y + y1), 0xffffff);
+		float cx = frame.x + frame.w / 2.0f;
+		float cy = frame.y + frame.h / 2.0f;
+		float r = 1.5f*frame.w / 2;
+		float c = Jogo::cosine(-roll * Jogo::D2R);
+		float s = Jogo::sine(-roll * Jogo::D2R);
+		
+		s32 q;
+		float p;
+		Jogo::remainder(-pitch, 180.0f, 1.0f / 180.0f, q, p);
+
+		float upx = s;
+		float upy = -c;
+		// are we upside-down?
+		//if (q & 1)
+		//{
+		//	upx = -s;
+		//	upy = c;
+		//}
+
+		// find x,y where pitch is 0
+		//if (q & 1)
+		//{
+		//	PitchOriginScreenSpaceX = cx + p * PitchToScreenScale * upx;
+		//	PitchOriginScreenSpaceY = cy - p * PitchToScreenScale * upy;
+		//}
+		//else
+		{
+			PitchOriginScreenSpaceX = cx + p * PitchToScreenScale * upx;
+			PitchOriginScreenSpaceY = cy + p * PitchToScreenScale * upy;
+		}
+
+		// are we upside-down?
+		if (q & 1)
+		{
+			upx = -s;
+			upy = c;
+		}
+
+		// find the endpoints of the horizon line in screen space
+		s32 x1 = (s32)(PitchOriginScreenSpaceX - r * c);
+		s32 y1 = (s32)(PitchOriginScreenSpaceY - r * s);
+		s32 x2 = (s32)(PitchOriginScreenSpaceX + r * c);
+		s32 y2 = (s32)(PitchOriginScreenSpaceY + r * s);
+
+		// for each horizontal line of the display
+		// determine where the line segment intersects the frame
+		// then go from left side to the right with one color, then the other
+
+		// determine the first line of sky, which would be 
+		u32 color = SkyColor;
+		for (s32 screeny = frame.y; screeny < frame.y + frame.h; screeny++)
+		{
+			// is this line sky or ground?
+			float testx = (float)frame.x;
+			float testy = (float)screeny;
+			VectorToPitchOriginScreenSpace(testx, testy);
+
+			float LeftDot = testx * upx + testy * upy;
+			color = LeftDot > 0 ? SkyColor : GroundColor;
+			// TODO: coompute where sky turns to ground etc.
+			testx = (float)(frame.x + frame.w);
+			testy = (float)screeny;
+			VectorToPitchOriginScreenSpace(testx, testy);
+			float RightDot = testx * upx + testy * upy;
+			s32 RightEdge = frame.x + frame.w;
+			if ((RightDot >= 0 && LeftDot < 0) || (RightDot < 0 && LeftDot >= 0))
+			{
+				// compute the intersection of the horizon line with current scanline
+				RightEdge = frame.x + (s32)(LeftDot * frame.w / (LeftDot - RightDot));
+			}
+			BackBuffer.DrawHLine(screeny, frame.x, RightEdge, color);
+			color = color == SkyColor ? GroundColor: SkyColor;
+			BackBuffer.DrawHLine(screeny, RightEdge, frame.x + frame.w, color);
+		}
+		// draw the horizon line
+
+		if (BackBuffer.ClipLine(x1, y1, x2, y2, frame))
+		{
+			BackBuffer.DrawLine(x1, y1, x2, y2, 0xffffff);
+
+			//s32 pox = PitchOriginScreenSpaceX;
+			//s32 poy = PitchOriginScreenSpaceY;
+		}
+
+		BackBuffer.DrawCircle((s32)cx, (s32)cy, 10, 0xffffff);
+
+		x1 = (s32)cx;	// PitchOriginScreenSpaceX;
+		y1 = (s32)cy;	// PitchOriginScreenSpaceY;
+		x2 = x1 + (s32)(upx * 30);
+		y2 = y1 + (s32)(upy * 30);
+		BackBuffer.DrawLine(x1, y1, x2, y2, 0xffff);
+		char pitchString[32];
+		Jogo::itoa((int)pitch%360, pitchString, 32);
+		DefaultFont.DrawText(0, 0, pitchString, 0xffffff, BackBuffer);
 	}
 
 	void DrawSineWave()
@@ -75,7 +224,7 @@ public:
 		s32 ox = BackBuffer.Width / 2;
 		s32 oy = BackBuffer.Height / 2;
 		int x1=0, y1=oy;
-		for (int x = 0; x < BackBuffer.Width; x++)
+		for (s32 x = 0; x < (s32)BackBuffer.Width; x++)
 		{
 			
 			float a = (x/scale - ox) * Jogo::D2R - (originx + deltax)/100.f;
@@ -94,10 +243,10 @@ public:
 
 		Bitmap::Rect horizonBox = { 250,250,500,500 };
 		DrawHorizon(horizonBox, pitch, roll);
-		DrawSineWave();
-		char scrollString[32];
-		Jogo::itoa(scroll, scrollString);
-		DefaultFont.DrawText(0,0,scrollString, 0xffffff, BackBuffer);
+		//DrawSineWave();
+		//char scrollString[32];
+		//Jogo::itoa(scroll, scrollString);
+		//DefaultFont.DrawText(0,0,scrollString, 0xffffff, BackBuffer);
 
 		Jogo::Show(BackBuffer.PixelBGRA, BackBuffer.Width, BackBuffer.Height);
 	}
@@ -105,7 +254,8 @@ public:
 
 const char* Horizon::Name = "Horizon";
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
+
 {
 	Horizon horizon;
 	Jogo::Run(horizon, 60);
